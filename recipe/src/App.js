1a60e5e4fcc66592a76652b0dfb327062b2ae951
Yifei Zhou <yfzhou23@gmail.com>
2024-01-24 19:20:50 +0000

    Create project, add title and icons

---
+++ recipe/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import { AiOutlineRest } from "react-icons/ai";
+import './App.css';
+
+const Header = () => {
+  return (
+    <header className="app-header">
+      <div className="header-content">
+        <AiOutlineRest style={{ fontSize: '200%'}}/>
+        <h1>Recipe Hub</h1>
+      </div>
+    </header>
+  );
+};
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      {/* Your other components and content go here */}
+    </div>
+  );
+};
+
+export default App;

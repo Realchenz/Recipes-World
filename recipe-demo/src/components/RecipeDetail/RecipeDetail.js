58fc42090a30d533d6110a93be0792da1b9f8771
Yifei Zhou <yfzhou23@gmail.com>
2024-02-04 06:23:46 +0000

    Add route link for each recipe, experiment 1

--- recipe-demo/src/components/RecipeDetail/RecipeDetail.js
+++ recipe-demo/src/components/RecipeDetail/RecipeDetail.js
@@ -1,24 +1,43 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './RecipeDetail.scss';
+import Container from 'react-bootstrap/esm/Container';
+import Row from 'react-bootstrap/esm/Row';
 
-const RecipeDetail = props => (
-	<div>This is a component called RecipeDetail.</div>
-);
-
-// todo: Unless you need to use lifecycle methods or local state,
-// write your component in functional form as above and delete
-// this section. 
-// class RecipeDetail extends React.Component {
-//   render() {
-//     return <div>This is a component called RecipeDetail.</div>;
-//   }
-// }
-
-const RecipeDetailPropTypes = {
-	// always use prop types!
+const RecipeDetail = ({ recipe }) => {
+	
+	return(
+		<Container>
+			<Row>
+				<h3>{recipe.title}</h3>
+			</Row>
+			<Row>
+				<Col xs={8}>
+				<div>
+					<p className="text-muted" style={{ wordWrap: 'break-word', fontSize: '14pt', fontStyle: 'italic' }}>{selectedRecipe.description}</p>
+					<h3 style={{ fontSize: '24pt' }}>Ingredients:</h3>
+					<ul>
+					{selectedRecipe.ingredients.map((ingredient, index) => (
+					<li key={index} style={{ fontSize: '14pt' }}>{ingredient}</li>
+					))}
+					</ul>
+					<h3 style={{ fontSize: '24pt' }}>Instructions:</h3>
+					<p className="text-muted" style={{ wordWrap: 'break-word', fontSize: '14pt' }}>{selectedRecipe.instructions}</p>
+				</div>
+				</Col>
+				<Col>
+				<div>
+					<img 
+					variant="top"
+					src={selectedRecipe.image}
+					alt={selectedRecipe.title}
+					style={{ width: '300px', height: '300px', marginBottom: '10px' }} />
+				</div>
+				</Col>
+			</Row>
+		</Container>
+	)
+	
 };
-
-RecipeDetail.propTypes = RecipeDetailPropTypes;
 
 export default RecipeDetail;

0b89a568984940c9941532f3d01a316f8f0b7115
chen zhenxu <zachchandut@gmail.com>
2024-02-06 15:36:00 -0500

    WIP: 363dfc1 Merge pull request #33 from Ailuruscp/31-build-website-for-each-recipe

--- recipe-demo/src/components/RecipeDetail/RecipeDetail.test.js
+++ recipe-demo/src/components/RecipeDetail/RecipeDetail.test.js
@@ -1,43 +1,49 @@
 import React from 'react';
-import { shallow, render, mount } from 'enzyme';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect'; 
+import { BrowserRouter, MemoryRouter, Route, Routes } from 'react-router-dom';
+
 import RecipeDetail from './RecipeDetail';
 
-describe('RecipeDetail', () => {
-  let props;
-  let shallowRecipeDetail;
-  let renderedRecipeDetail;
-  let mountedRecipeDetail;
-
-  const shallowTestComponent = () => {
-    if (!shallowRecipeDetail) {
-      shallowRecipeDetail = shallow(<RecipeDetail {...props} />);
+describe('RecipeDetail Component', () => {
+  const recipes = [
+    {
+      title: "Spaghetti Carbonara",
+      id: 1,
+      description: "A classic Italian pasta dish with eggs, cheese, pancetta, and pepper.",
+      ingredients: ["Spaghetti", "Eggs", "Parmesan Cheese", "Pancetta", "Black Pepper"],
+      instructions: "Cook pasta. In separate pan, cook pancetta. Whisk eggs and Parmesan, then combine with pasta and pancetta. Season with pepper.",
+      image: "https://media.istockphoto.com/id/177413384/photo/pasta-with-carbonara.jpg?s=1024x1024&w=is&k=20&c=5ZRLITJjvwm0aEV6ynpdnJrjDmlraH-PD9mleSts6sQ="
     }
-    return shallowRecipeDetail;
-  };
+  ];
 
-  const renderTestComponent = () => {
-    if (!renderedRecipeDetail) {
-      renderedRecipeDetail = render(<RecipeDetail {...props} />);
-    }
-    return renderedRecipeDetail;
-  };
+  it('renders recipe detail correctly', () => {
+    const id = '1';
+    const recipe = recipes.find(recipe => String(recipe.id) === id);
 
-  const mountTestComponent = () => {
-    if (!mountedRecipeDetail) {
-      mountedRecipeDetail = mount(<RecipeDetail {...props} />);
-    }
-    return mountedRecipeDetail;
-  };  
+    render(
+      <MemoryRouter initialEntries={[`/recipes/${id}`]}>
+        <Routes>
+          <Route path="/recipes/:id"  element={<RecipeDetail recipes={recipes} />} />
+        </Routes>
+      </MemoryRouter>
+    );
 
-  beforeEach(() => {
-    props = {};
-    shallowRecipeDetail = undefined;
-    renderedRecipeDetail = undefined;
-    mountedRecipeDetail = undefined;
+    const recipeTitle = screen.getByText(recipe.title);
+    expect(recipeTitle).toBeInTheDocument();
+
+    const recipeDescription = screen.getByText(recipe.description);
+    expect(recipeDescription).toBeInTheDocument();
+
+    const recipeImage = screen.getByAltText(recipe.title);
+    expect(recipeImage).toBeInTheDocument();
+
+    recipe.ingredients.forEach((ingredient) => {
+      const ingredientElement = screen.getByText(ingredient);
+      expect(ingredientElement).toBeInTheDocument();
+    });
+
+    const recipeInstructions = screen.getByText(recipe.instructions);
+    expect(recipeInstructions).toBeInTheDocument();
   });
-
-  // Shallow / unit tests begin here
- 
-  // Render / mount / integration tests begin here
-  
 });

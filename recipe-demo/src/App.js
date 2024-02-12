e32ab31eb43e318f475948a13a77397154ef8733
Yifei Zhou <yfzhou23@gmail.com>
2024-02-12 22:07:54 +0000

    Add Dropdown recipes menu in the Nav bar

--- recipe-demo/src/App.js
+++ recipe-demo/src/App.js
@@ -7,6 +7,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 const App = () => {
 
@@ -90,6 +91,11 @@ const App = () => {
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="me-auto">
                 <Nav.Link href="/team">Team Page</Nav.Link>
+                <NavDropdown title="Recipes Links" id="basic-nav-dropdown">
+                  {recipes.map((recipe) => (
+                    <NavDropdown.Item href={"/recipes/" + recipe.id}>{recipe.title}</NavDropdown.Item>
+                  ))}
+                </NavDropdown>
               </Nav>
             </Navbar.Collapse>
           </Container>

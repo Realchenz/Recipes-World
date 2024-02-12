9a5e04a1a1484ca5acbdf44c5568bc2956b8311c
Yifei Zhou <yfzhou23@gmail.com>
2024-02-12 21:54:58 +0000

    Add Team Page on Nav Bar

--- recipe-demo/src/App.js
+++ recipe-demo/src/App.js
@@ -88,10 +88,12 @@ const App = () => {
         </header>
         <Navbar expand="lg" className="bg-body-tertiary">
           <Container>
-            <Navbar.Brand href="/">Home</Navbar.Brand>
+            <Navbar.Brand href="/">Home Page</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
-              
+              <Nav className="me-auto">
+                <Nav.Link href="/team">Team Page</Nav.Link>
+              </Nav>
             </Navbar.Collapse>
           </Container>
         </Navbar>

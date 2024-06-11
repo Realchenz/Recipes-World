7983f400bd58cdb8e7cdcf433224d353b97be960
chen zhenxu <zachchandut@gmail.com>
2024-06-11 00:38:39 -0400

    phased change

--- recipe-frontend/src/components/AddRecipe/AddRecipe.js
+++ recipe-frontend/src/components/AddRecipe/AddRecipe.js
@@ -7,7 +7,7 @@ function AddRecipe() {
     description: '',
     ingredients: '',
     instructions: '',
-    imageURL: ''
+    image: ''
   });
 
   const handleChange = (e) => {
@@ -25,70 +25,75 @@ function AddRecipe() {
   };
 
   return (
-    <form className="recipe-form" onSubmit={handleSubmit}>
-      <h2>Input Recipe Info</h2>
-      
+    <div className='add-recipe'>
+      <form className="recipe-form" onSubmit={handleSubmit}>
+        <h2>Input Recipe Info</h2>
+        
+        <div className="form-group">
+          <label htmlFor="title">Title</label>
+          <input 
+              type="text" 
+              id="title" 
+              name="title" 
+              value={formData.title} 
+              onChange={handleChange} 
+              required 
+          />
+        </div>
+        
+
       <div className="form-group">
-        <label htmlFor="title">Title</label>
-        <input 
-            type="text" 
-            id="title" 
-            name="title" 
-            value={formData.title} 
-            onChange={handleChange} 
-            required 
+          <label htmlFor="description">Description</label>
+          <input
+              type="text" 
+              id="description" 
+              name="description" 
+              value={formData.description} 
+              onChange={handleChange} 
+              required 
+          />
+      </div>
+        
+      <div className="form-group">
+        <label htmlFor="ingredients">Ingredients</label>
+        <input
+          type="text"
+          id="ingredients" 
+          name="ingredients" 
+          value={formData.ingredients} 
+          onChange={handleChange} 
+          placeholder="e.g., 2 cups of flour, 1/2 cup of sugar, etc. Separated with comma."
+          required 
         />
       </div>
-      
-
-    <div className="form-group">
-        <label htmlFor="description">Description</label>
+        
+      <div className="form-group">
+        <label htmlFor="instructions">Instructions</label>
         <input
-            type="text" 
-            id="description" 
-            name="description" 
-            value={formData.description} 
-            onChange={handleChange} 
-            required 
+          type="text"
+          id="instructions" 
+          name="instructions" 
+          value={formData.instructions} 
+          onChange={handleChange} 
+          placeholder="e.g., Mix flour and sugar, Bake at 350 degrees, etc. Separated with comma."
+          required 
         />
+      </div>
+        
+      <div className="form-group">
+        <label htmlFor="image">ImageURL</label>
+        <input 
+          type="text" 
+          id="image" 
+          name="image" 
+          onChange={handleChange} 
+          value={formData.image}
+        />
+      </div>
+        
+        <button type="submit">Submit Recipe</button>
+      </form>
     </div>
-      
-    <div className="form-group">
-      <label htmlFor="ingredients">Ingredients</label>
-      <input
-        type="text"
-        id="ingredients" 
-        name="ingredients" 
-        value={formData.ingredients} 
-        onChange={handleChange} 
-        required 
-      />
-    </div>
-      
-    <div className="form-group">
-      <label htmlFor="instructions">Instructions</label>
-      <input
-        type="text"
-        id="instructions" 
-        name="instructions" 
-        value={formData.instructions} 
-        onChange={handleChange} 
-        required 
-      />
-    </div>
-      
-    <div className="form-group">
-      <label htmlFor="imageURL">Image</label>
-      <input 
-        type="text" 
-        id="imageURL" 
-        name="imageURL" 
-        onChange={handleChange} 
-      />
-    </div>
-      
-      <button type="submit">Submit Recipe</button>
-    </form>
   );
 }
 

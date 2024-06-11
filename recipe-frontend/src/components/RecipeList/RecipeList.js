a895e8de19b543b0d723b4e42dd6f9fa547ff797
chen zhenxu <zachchandut@gmail.com>
2024-06-11 15:51:09 -0400

    realize pagination function

--- recipe-frontend/src/components/RecipeList/RecipeList.js
+++ recipe-frontend/src/components/RecipeList/RecipeList.js
@@ -1,35 +1,52 @@
+import React, { useState } from 'react';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import { Link } from 'react-router-dom';
 
-const RecipeList = ( {recipes} ) => {
+const RecipeList = ({ recipes }) => {
+  const itemsPerPage = 8;
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const lastRecipeIndex = currentPage * itemsPerPage;
+  const firstRecipeIndex = lastRecipeIndex - itemsPerPage;
+  const currentRecipes = recipes.slice(firstRecipeIndex, lastRecipeIndex);
+
+  const totalPages = Math.ceil(recipes.length / itemsPerPage);
 
   return (
-      <Container>
-        <Row>
-          <div style={{ display: 'flex', flexWrap: 'wrap' }}>
-            {recipes.map((recipe) => (
-              <Card key={recipe.id} style={{ width: '18rem', height: '25rem', margin: '10px', backgroundColor: 'black', border: '1px solid white' }}>
-                <div className="d-flex justify-content-center align-items-center">
-                  <Card.Img variant="top" src={recipe.image} alt={recipe.title} style={{ width: '17rem', height: '18rem', marginBottom: '10px' }} />
-                </div>
-                <Card.Body>
-                  <Card.Title style={{ fontSize: '1.2rem', marginBottom: '10px', color: 'white' }}>{recipe.title}</Card.Title>
-                  <Link to={`/recipes/${recipe.id}`}>
-                    <Button 
-                      style={{ padding: '6px 10px', fontSize: '1.0rem', marginBottom: '10px', backgroundColor: 'white', color: 'black' }}
-                      variant="primary" index={recipe.id}>
-                      Details
-                    </Button>
-                  </Link>
-                </Card.Body>
-              </Card>
-            ))}
-          </div>
-        </Row>
-      </Container>
+    <Container>
+      <Row>
+        <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+          {currentRecipes.map((recipe) => (
+            <Card key={recipe.id} style={{ width: '18rem', height: '25rem', margin: '10px', backgroundColor: 'black', border: '1px solid white' }}>
+              <div className="d-flex justify-content-center align-items-center">
+                <Card.Img variant="top" src={recipe.image} alt={recipe.title} style={{ width: '17rem', height: '18rem', marginBottom: '10px' }} />
+              </div>
+              <Card.Body>
+                <Card.Title style={{ fontSize: '1.2rem', marginBottom: '10px', color: 'white' }}>{recipe.title}</Card.Title>
+                <Link to={`/recipes/${recipe.id}`}>
+                  <Button style={{ padding: '6px 10px', fontSize: '1.0rem', marginBottom: '10px', backgroundColor: 'white', color: 'black' }} variant="primary" index={recipe.id}>
+                    Details
+                  </Button>
+                </Link>
+              </Card.Body>
+            </Card>
+          ))}
+        </div>
+      </Row>
+      <Row>
+        <div style={{ display: 'flex', justifyContent: 'center', marginTop: '20px' }}>
+          <Button onClick={() => setCurrentPage(currentPage - 1)} disabled={currentPage === 1}>
+            Previous
+          </Button>
+          <Button onClick={() => setCurrentPage(currentPage + 1)} disabled={currentPage === totalPages}>
+            Next
+          </Button>
+        </div>
+      </Row>
+    </Container>
   );
 };
 
